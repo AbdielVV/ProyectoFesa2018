ca9643aa2cc11bedefea3b3d57eb143951ac1e7d
AbdielVV <37086377+AbdielVV@users.noreply.github.com>
2018-08-01 20:54:13 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'id3836858_abdiel');
+define('DB_PASSWORD', 'yesika');
+define('DB_NAME', 'id3836858_registro');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
